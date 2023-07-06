c4cb50070082069b98d2db2b7c0fe67dff3b0618
ianemiranda <133229803+ianemiranda@users.noreply.github.com>
2023-07-06 13:26:50 +0000

    oi

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = 'Agora são $ {hora} horas.'
+msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12){
     // BOM DIA!
     img.src = 'fotomanha.png.png'
